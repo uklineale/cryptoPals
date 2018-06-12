b69c4adef1050a9d1057d760875154e66c0ed77c
Neel Kuila <nkuila@bandwidth.com>
2018-06-11 20:15:33 -0400

    add edge case, start seventeen

--- python/set2/fifteen.py
+++ python/set2/fifteen.py
@@ -2,7 +2,7 @@ from python.set2.nine import unpad_pkcs7
 
 # Just edited the unpad function in 9
 if __name__ == '__main__':
-    fails = [b'YELLOW SUBMARINE\x01\x02\x03', b'your buy tencent\x02', b'YELLOW SUBMARINE'+ b'\x00' * 16]
+    fails = [b'YELLOW SUBMARINE\x01\x02\x03', b'your buy tencent\x02', b'YELLOW SUBMARI'+ b'\x02' + b'\x01']
     for i in fails:
         try:
             unpad_pkcs7(i)
