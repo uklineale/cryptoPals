a9223438eae801e83d887d18805f8f7c29f314b5
Neel Kuila <nkuila@bandwidth.com>
2018-11-13 21:46:10 -0500

    add freq, solve nineteen and twenty in one fell swoop

--- python/common/util.py
+++ python/common/util.py
@@ -4,7 +4,7 @@ def binary_assert_equals(bstr1, bstr2, printStats=False):
         print("Length binary 2: " + str(len(bstr2)))
         for i in range(len(bstr1)):
             if bstr1[i] != bstr2[i]:
-                print("Byte {} \nExpected: {} | Actual: {}".format(i, bstr1[i], bstr2[i]))
+                print("Byte {} \nExpectFed: {} | Actual: {}".format(i, bstr1[i], bstr2[i]))
 
     assert bstr1 == bstr2
 
