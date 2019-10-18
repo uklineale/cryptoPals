c2ed3d9be3c6be40c5cdd20cab18b3644040ebd4
Neel Kuila <nkuila@bandwidth.com>
2019-10-18 17:43:11 -0400

    solved c25

--- rust/src/aes.rs
+++ rust/src/aes.rs
@@ -7,19 +7,29 @@ use rand::Rng;
 use std::io::Cursor;
 use std::str::from_utf8;
 
+const DEFAULT_KEY: &[u8] = b"YELLOW SUBMARINE";
+const BYTE_SIZE: usize = 8;
+
 pub struct MyCiphers {
     core: Cipher,
-    key: String,
+    key: Vec<u8>,
     block_size: usize,
 }
 
 impl MyCiphers {
-    pub fn init() -> MyCiphers {
-        let key = "YELLOW SUBMARINE".to_string();
+    pub fn init_default() -> MyCiphers {
         MyCiphers {
             core: Cipher::aes_128_ecb(),
-            block_size: key.len(),
-            key,
+            block_size: DEFAULT_KEY.len() * BYTE_SIZE,
+            key: DEFAULT_KEY.to_vec(),
+        }
+    }
+
+    pub fn init(key: Vec<u8>) -> MyCiphers {
+        MyCiphers {
+            core: Cipher::aes_128_ecb(),
+            block_size: key.len() * BYTE_SIZE,
+            key
         }
     }
 
@@ -27,54 +37,71 @@ impl MyCiphers {
         self.core
     }
 
-    pub fn getKey(&self) -> &String {
+    pub fn getKey(&self) -> &Vec<u8> {
         &self.key
     }
 
     pub fn xcryptCtr(&self, text: &[u8], nonce: u64) -> Vec<u8> {
         let mut counter: u64 = 0;
-        let mut result: Vec<u8> = vec![];
-        let num_blocks = num_blocks(self.key.as_bytes(), text);
+        let keystream = self.generateKeystream(text, nonce);
 
-        for block_num in 0..num_blocks {
-            // Is every bit equally likely (statistically)?
-            let keystream = self.generateKeystream(nonce, counter, self.key.as_bytes().to_vec());
-            counter += 1;
-            let block_start = block_num * self.block_size;
-            let block = &text[block_start..block_start + self.block_size];
-
-            let mut crypted: Vec<u8> = keystream
-                .iter()
-                .zip(block.iter())
-                .map(|bytes: (&u8, &u8)| -> u8 { bytes.0 ^ bytes.1 })
-                .collect();
-
-            result.append(&mut crypted);
-        }
-        println!("len of result: {}", result.len());
-
-        return result;
+        keystream
+            .iter()
+            .zip(text.iter())
+            .map(|bytes: (&u8, &u8)| -> u8 { bytes.0 ^ bytes.1 })
+            .collect()
     }
 
-    pub fn generateKeystream(&self, nonce: u64, ctr: u64, key: Vec<u8>) -> Vec<u8> {
-        let nonce_and_counter = [nonce.to_le_bytes(), ctr.to_le_bytes()].concat();
-        self.encryptEcb(nonce_and_counter.as_ref())
+    pub fn generateKeystream(&self, text: &[u8], nonce: u64) -> Vec<u8> {
+        let num_blocks = num_blocks(&self.key, text);
+        let mut counter: u64 = 0;
+        let mut keystream = vec![];
+
+        for block_num in 0..num_blocks {
+            let nonce_and_counter = [nonce.to_le_bytes(), counter.to_le_bytes()].concat();
+            let mut stream_block = self.encryptEcb(nonce_and_counter.as_ref());
+            keystream.append(&mut stream_block);
+            counter += 1;
+        }
+
+        keystream.truncate(text.len());
+
+        return keystream;
     }
 
     pub fn encryptEcb(&self, pt: &[u8]) -> Vec<u8> {
         // todo figure out Result handling
-        encrypt(self.core, self.key.as_bytes(), None, &pt).unwrap()
+        encrypt(self.core, &self.key, None, &pt).unwrap()
     }
 
-    pub fn decryptEcb(self, ct: &[u8]) -> Vec<u8> {
+    pub fn decryptEcb(&self, ct: &[u8]) -> Vec<u8> {
         // todo figure out Result handling
-        decrypt(self.core, self.key.as_bytes(), None, &ct).unwrap()
+        decrypt(self.core, &self.key, None, &ct).unwrap()
+    }
+
+    //s4c25 - CTR random access read/write
+    pub fn edit(&self, ct: &mut [u8], nonce: u64, offset: usize, new_text: &[u8]) -> Vec<u8> {
+        let keystream = self.generateKeystream(ct, nonce);
+        let end_offset = offset + new_text.len();
+        let keystream_subset = &keystream[offset..end_offset];
+
+        let mut new_ct: Vec<u8> = new_text.to_vec()
+            .iter()
+            .zip(keystream_subset.iter())
+            .map(|bytes: (&u8, &u8)| -> u8 { bytes.0 ^ bytes.1 })
+            .collect();
+
+        for i in 0..new_text.len() {
+            ct[i+offset] = new_ct[i];
+        }
+
+        return ct.to_vec();
     }
 }
 
 #[test]
 fn test_encrypt_decrypt_ecb() {
-    let cipher = MyCiphers::init();
+    let cipher = MyCiphers::init_default();
 
     let pt = b"this is 32 charsthis is 32 chars";
     let ct = cipher.encryptEcb(pt);
@@ -85,7 +112,7 @@ fn test_encrypt_decrypt_ecb() {
 
 #[test]
 fn test_encrypt_decrypt_ctr() {
-    let cipher = MyCiphers::init();
+    let cipher = MyCiphers::init_default();
     let mut rng = rand::thread_rng();
     let nonce: u64 = rng.next_u64();
 
--- rust/src/s4c25.rs
+++ rust/src/s4c25.rs
@@ -1,20 +1,45 @@
 mod aes;
 
 use aes::MyCiphers;
-use hex::encode;
+use hex::{encode, decode};
 use rand::Rng;
+use std::fs;
+use std::str::from_utf8;
+use std::borrow::BorrowMut;
+
+// This is inefficient. You can set the plaintext to the ciphertext,
+// then CTR will decrypt it for you!
+pub fn crack_random_access_ctr(cipher: &MyCiphers, ct: &mut [u8], nonce: u64) -> Vec<u8> {
+    let mut cracked: Vec<u8> = vec!();
+
+    for i in 0..ct.len() {
+        let target = ct[i].clone();
+        for guess in 0..255u8 {
+            cipher.edit(ct, nonce, i, &[guess]);
+            if target == ct[i] {
+                cracked.extend_from_slice(&[guess]);
+                break;
+            }
+        }
+    }
+
+    return cracked;
+}
 
 pub fn main() {
-    let cipher = MyCiphers::init();
-
     let mut rng = rand::thread_rng();
     let nonce: u64 = rng.next_u64();
 
-    // 128 bytes
-    let pt = b"this is 32 charsthis is 32 chars";
+    let mut key_bytes: [u8;16] = [0; 16];
+    rng.fill_bytes(&mut key_bytes);
+    let key = key_bytes.to_vec();
 
-    let ct = cipher.xcryptCtr(pt, nonce);
-    let result = cipher.xcryptCtr(&ct, nonce);
+    let c = MyCiphers::init(key);
 
-    print!("expected: {:?}\nresult:   {:?}", encode(pt), encode(result));
+    let pt = b"This is a secret. Have I successfully decrypted this?";
+
+    let mut ct = c.xcryptCtr(pt, nonce);
+    let ct_clone = ct.clone();
+    let result = c.edit(&mut ct, nonce, 0,  &ct_clone);
+    println!("{}", from_utf8(&result).unwrap());
 }
